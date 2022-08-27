d1e4c18b745d73f55c1eff8ecf1234281417983d
Hugo Prada <111398370+miprago@users.noreply.github.com>
2022-08-27 11:33:31 -0500

    sprint2
    construccion de clases. de acuerdo a la solicitud.. para ser complementadas por el equipo

---
+++ sprint2_Global_line/src/test/java/com/ciclo3/sprint2_Global_line/Sprint2GlobalLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.sprint2_Global_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2GlobalLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
