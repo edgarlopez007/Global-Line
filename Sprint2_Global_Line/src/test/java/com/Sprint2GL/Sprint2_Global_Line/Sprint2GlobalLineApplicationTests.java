0722cab64cca26fbe0528c33fbdcfba529a830bb
Hugo Prada <111398370+miprago@users.noreply.github.com>
2022-08-30 19:22:21 -0500

    Creación de clases
    se realiza creación de clases de acuerdo a requerimientos de lms. pendiente de cambios por parte del equipo.

---
+++ Sprint2_Global_Line/src/test/java/com/Sprint2GL/Sprint2_Global_Line/Sprint2GlobalLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sprint2GL.Sprint2_Global_Line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2GlobalLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
