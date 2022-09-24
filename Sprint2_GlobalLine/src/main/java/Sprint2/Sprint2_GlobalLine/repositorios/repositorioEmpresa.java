ba7a7538ea379c370c10b62a3cbdfaeb3604ea76
Juan Rodriguez <juandavidrh02@gmail.com>
2022-09-24 18:38:53 -0500

    sprint3

---
+++ Sprint2_GlobalLine/src/main/java/Sprint2/Sprint2_GlobalLine/repositorios/repositorioEmpresa.java
@@ -0,0 +1,7 @@
+package Sprint2.Sprint2_GlobalLine.repositorios;
+
+import Sprint2.Sprint2_GlobalLine.entidades.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface repositorioEmpresa extends JpaRepository<Empresa, Long> {
+}
