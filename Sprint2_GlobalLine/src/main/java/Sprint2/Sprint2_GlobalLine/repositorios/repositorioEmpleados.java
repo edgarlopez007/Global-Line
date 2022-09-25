577cdcc0d73f54bea3e8f0a035efa185cc5eb427
Juan Rodriguez <juandavidrh02@gmail.com>
2022-09-24 19:11:43 -0500

    sprint3

--- Sprint2_GlobalLine/src/main/java/Sprint2/Sprint2_GlobalLine/repositorios/repositorioEmpleados.java
+++
@@ -1,7 +0,0 @@
-package Sprint2.Sprint2_GlobalLine.repositorios;
-
-import Sprint2.Sprint2_GlobalLine.entidades.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repositorioEmpleados extends JpaRepository<Empleado, Long> {
-}
--- Sprint2_GlobalLine/src/main/java/Sprint2/Sprint2_GlobalLine/repositorios/repositorioTransacciones.java
+++
@@ -1,7 +0,0 @@
-package Sprint2.Sprint2_GlobalLine.repositorios;
-import Sprint2.Sprint2_GlobalLine.entidades.Transacciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface repositorioTransacciones extends JpaRepository<Transacciones, Long>{
-}
